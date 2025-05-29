ad69114f03761b861a5648d0b6bd0588a767f23d
Abhishek panchal <abhipan.ap33@gmail.com>
2025-05-29 17:46:06 +0530

    create server and send response

--- first-nodejs-code/server.mjs
+++ first-nodejs-code/server.mjs
@@ -1,3 +1,5 @@
+// This is a simple Node.js server that responds with HTML content.
+
 import { createServer } from "node:http";
 
 const hostname = "127.0.0.1";
@@ -5,7 +7,7 @@ const port = 3000;
 
 const server = createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  res.setHeader("content-type", "text/plain");
+  res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Node.js Server</title></head>");
   res.write("<body>");
@@ -13,6 +15,7 @@ const server = createServer((req, res) => {
   res.write("<p>This is my first Node.js server!</p>");
   res.write("</body>");
   res.write("</html>");
+  res.end();
 });
 
 server.listen(port, hostname, () => {
